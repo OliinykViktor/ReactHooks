88be90a7a0a79e681eb3a3604d2b7b79a8209a0a
Viktor <oliinyk.v.7.7@gmail.com>
2023-08-05 00:22:56 +0300

    useState - hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import {Container} from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
 
 const Slider = () => {
+
+    const [state, setState] = useState({
+            slide: 0,
+            autoplay:false,
+        })
+
+    const toggleAutoplay = () => {
+        setState((prevState) => ({
+            ...prevState,
+            autoplay: !prevState.autoplay,
+        }))
+    }
+
+    const changeSlide = (value) => {
+        setState((prevState) => ({
+            ...prevState,
+            slide : prevState.slide + value,
+        }))
+    }
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide <br/></div>
+                <div className="text-center mt-5">Active slide {state.slide} <br/> {state.autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        >-1</button>
+                        onClick={() => changeSlide(-1)}>-1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        >+1</button>
+                        onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        >toggle autoplay</button>
+                        onClick={toggleAutoplay}>toggle autoplay</button>
                 </div>
             </div>
         </Container>
